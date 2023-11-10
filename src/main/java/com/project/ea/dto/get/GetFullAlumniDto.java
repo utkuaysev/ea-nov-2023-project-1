cdddcab0ca64dcb93dd3f1ed68a2bdb1dbd25207
Bacha Wondimu <bwondim3@Montgomerycollege.edu>
2023-11-09 21:46:17 -0600

    changed AlumniRepo to AlumniRepository

--- src/main/java/com/project/ea/dto/get/GetFullAlumniDto.java
+++ src/main/java/com/project/ea/dto/get/GetFullAlumniDto.java
@@ -1,4 +1,13 @@
 package com.project.ea.dto.get;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GetFullAlumniDto {
+    private String mail;
+    private String password;
+    private String phone;
+    private String profilePicture;
 }
